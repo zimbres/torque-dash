3e74b2ff43005eb917f679ec3245521427553463
Marcio <marcio@zimbres.com>
2025-05-02 21:17:57 -0300

    Fix to be able to get session keys from env

--- config/config.js
+++ config/config.js
@@ -7,7 +7,9 @@ let config = {
         }
     },
     session: {
-        keys: process.env.SESSION_KEYS || ['6a5w4d65a4wd', 'a65w4d6aw4d89a4', '65f4b8b4szd8']
+        keys: process.env.SESSION_KEYS 
+            ? process.env.SESSION_KEYS.split(',') 
+            : ['6a5w4d65a4wd', 'a65w4d6aw4d89a4', '65f4b8b4szd8']
     }
 };
 
